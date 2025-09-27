8f3714ccf9f1fce57489572fb4f69b1d33225852
arjav_p <arjav.m.prabhu@gmail.com>
2025-09-27 14:00:59 -0400

    saw on l2

---
+++ Scripts/enemy.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@onready var player: CharacterBody2D = $"../Player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered")) # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_body_entered(body) -> void:
+	if body == player:
+		Globals.health -= 25
